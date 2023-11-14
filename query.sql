b0e4fbbb134e08a2e8217504b9ec4726d1c14b00
cy <93538794+hexcreator@users.noreply.github.com>
2023-11-14 12:05:06 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -5,11 +5,11 @@ SELECT
   bytearray_substring (data, 1, 4) as "method_id", -- expression, start, length
   bytearray_substring (data, 17, 20) as "tokenIn",
   bytearray_substring (data, 49, 20) as "tokenOut",
-  bytearray_to_int256(bytearray_substring (data, 99, 2)) as "fee",
+  bytearray_to_uint256(bytearray_substring (data, 99, 2)) as "fee",
   bytearray_substring (data, 113, 20) as "recipient", 
-  bytearray_to_int256(bytearray_substring (data, 161, 4)) as "deadline", 
-  bytearray_to_int256(bytearray_substring (data, 190, 4)) as "amountIn", 
-  bytearray_to_int256(bytearray_substring (data, 218, 4)) as "amountOutMinimum", 
+  bytearray_to_uint256(bytearray_substring (data, 161, 4)) as "deadline", 
+  bytearray_to_uint256(bytearray_substring (data, 190, 4)) as "amountIn", 
+  bytearray_to_uint256(bytearray_substring (data, 218, 4)) as "amountOutMinimum", 
   data
 FROM
   ethereum.transactions
